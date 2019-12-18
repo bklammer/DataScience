603972b13c47901a64c87d7ae2135a6fb1081fa8
bklammer <48070689+bklammer@users.noreply.github.com>
2019-12-18 22:03:55 +1300

    Initial Update
    Copied over some files that were in another folder

---
+++ iris.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 18 21:00:29 2019
+
+IRIS DATASET
+
+@author: benkl
+"""
+
+# required libraries
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from 
